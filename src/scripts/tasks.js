e16d8b278353ce6488622fdcc60d290bf59ac698
Jeremiah <jeremiah3643@gmail.com>
2018-05-16 13:33:14 -0500

    Dynamic button creation with form

--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -1,16 +1,34 @@
 const fragment = document.createDocumentFragment()
 const taskSection = document.querySelector(".taskSection")
-const taskInput = function () {
-    const inputName = document.createElement("input")
-    inputName.type = "text"
-    name.placeholder = "Task Name Here"
-    taskSection.appendChild(inputName)
-}
+const taskCreationField = document.querySelector(".taskfields")
 
 $(".taskCreationButton").on("click", function () {
     $(".taskCreationButton").hide()
     $(".taskSection").append(taskInput())
 })
+const taskInput = function () {
+    const taskDiv = document.createElement("div")
+    taskDiv.classList = "taskfield"
+    taskSection.appendChild(taskDiv)
+    const inputName = document.createElement("input")
+    inputName.type = "text"
+    inputName.placeholder = "Task Name Here"
+    taskDiv.appendChild(inputName)
+    const completionDate = document.createElement("input")
+    completionDate.type = "date"
+    completionDate.placeholder = "Date to Complete"
+    taskDiv.appendChild(completionDate)
+    const submitButton = document.createElement("button")
+    submitButton.textContent = "Submit"
+    submitButton.classList = "submitButton"
+    taskDiv.appendChild(submitButton)
+    //event listener attached to submit button and when submit button is pressed clears form and shows add task button
+    $(".submitButton").on("click", function () {
+        $(".taskfield").empty()
+        $(".taskCreationButton").show()})
+}
 
-
-
+/*$(".submitButton").on("click", function () {
+    $(".taskfield").empty()
+    $(".taskCreationButton").show()})
+*/
