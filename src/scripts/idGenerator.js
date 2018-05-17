2c4a982572433bdfd55251ca03a3b3c2e1d33f7e
dan beecroft <binauralbeat001@gmail.com>
2018-05-17 14:41:09 -0500

    registration now creates DB object and sets user ID in LS

--- src/scripts/idGenerator.js
+++
@@ -1,8 +0,0 @@
-const IdGenerator = (function(){ // creates IdGenerator function
-    var i=0; // sets index to 0
-    return function() {
-        return i++
-    }
-})();
-
-module.exports = IdGenerator
--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,23 +1,24 @@
-const IdGenerator = require("./idGenerator"); // require idGenerator.js
+// const IdGenerator = require("./idGenerator"); // require idGenerator.js
   $("#clickSub").click(function() { // click on <button class="clickSub"> to init function
 
 let userName= $("#userName").val() // make value of id="userName" into a variable
 console.log(userName) // console.log for varification
 let userEmail= $("#userEmail").val() // same as above but for email
 console.log(userEmail) // console.log for varification
-console.log(IdGenerator()) // console.log to verify value of IdGenerator
-const IDstring=JSON.stringify(IdGenerator())
-sessionStorage.setItem("userID", IDstring)
+// const IDstring=JSON.stringify(IdGenerator())
+// console.log(IDstring) // console.log to verify value of IdGenerator
+// sessionStorage.setItem("userID", IDstring)
+sessionStorage.setItem("username", userName)
 $.ajax({ // post to JSON DB
     url: "http://localhost:8088/users", // url where to submit the request
     method : "POST", // type of action POST
     data : { // object
-        "userID": IdGenerator(), // assign ID from required module
-        "username": (userName), // post userName
-        "email": (userEmail) // post email
+        // "userID": JSON.stringify(IdGenerator()), // assign ID from required module
+        "username": (JSON.stringify(userName)), // post userName
+        "email": (JSON.stringify(userEmail)) // post email
    }
 
- })
+ }).then(userID=> sessionStorage.setItem("userID", JSON.stringify(userID.id)))
 
 })
 
