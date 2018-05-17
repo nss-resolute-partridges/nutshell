8011a8b1f92843fe327ccc626cded69e74450128
rafcevallos <rafael.cevallos81@gmail.com>
2018-05-17 13:45:23 -0500

    Fixed API manager with Steve's suggested changes

--- src/scripts/APIManager.js
+++ src/scripts/APIManager.js
@@ -2,30 +2,31 @@ const $ = require("jquery")
 
 const APIManager = Object.create(null, {
 
-// Function to get all objects from the entire database regardless if it's User, News, Events, etc
-// obj is same as key
+    // Function to get all objects from the entire database regardless if it's User, News, Events, etc
+    // obj is same as key
     getAllObjects: {
         value: function (obj) {
             return $.ajax(`http://localhost:8088/${obj}`)
         }
     },
-// Function to get any SINGLE object from database.  For example, if you want to target a single news ID
+    // Function to get any SINGLE object from database.  For example, if you want to target a single news ID
     getSingleObject: {
         value: function (obj, id) {
             return $.ajax(`http://localhost:8088/${obj}/${id}`)
         }
     },
-// Function to CREATE objects in database.
+    // Function to CREATE objects in database.
+    // Added Steve's suggested changes
     createObject: {
-        value: function (obj) {
+        value: function (obj, objectToCreate) {
             return $.ajax({
-                url: `http://localhost:8088/${obj}`,
+                url: (`http://localhost:8088/${obj}`),
                 method: "POST",
-                data: `${obj}`
+                data: objectToCreate,
             })
         }
     },
-// Function to DELETE objects in database
+    // Function to DELETE objects in database
     deleteObject: {
         value: function (obj, id) {
             return $.ajax({
@@ -34,7 +35,7 @@ const APIManager = Object.create(null, {
             })
         }
     },
-// Function to UPDATE objects in database
+    // Function to UPDATE objects in database
     updateObject: {
         value: function (obj) {
             return $.ajax({
