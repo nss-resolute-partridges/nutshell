c6293b7670e031cc348a70a484e2e4aec3511130
Jeremiah <jeremiah3643@gmail.com>
2018-05-17 15:27:52 -0500

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,4 @@
 // REQUIRE all GENERATOR MODULES
-<<<<<<< HEAD
 const newsDisplay = require("./newsGenerator")
 const eventDisplay = require("./eventsGenerator")
 const friendDisplay = require("./friendsGenerator")
@@ -7,16 +6,7 @@ const taskDisplay = require("./tasksGenerator")
 const messageDisplay = require("./messagesGenerator")
 const tasks = require("./tasks")
 const APIManager = require("./APIManager")
-
-
-=======
-// const newsDisplay = require("./newsGenerator")
-// const eventDisplay = require("./eventsGenerator")
-// const friendDisplay = require("./friendsGenerator")
-// const taskDisplay = require("./tasksGenerator")
-// const messageDisplay = require("./messagesGenerator")
 const chatDisplay = require("./chat")
->>>>>>> bc06d6c819574a44c5157f9b8a2d4e9a5efe6e12
 // CALLS all GENERATOR functions
 // newsDisplay()
 // eventDisplay()
