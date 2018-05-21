57892f45c7ad86cd1a59436c1ac42233feeea0f7
Meghan Debity <mdebity@gmail.com>
2018-05-21 15:05:11 -0500

    quick update to get the 'add article' button on the DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,18 @@
 // REQUIRE all GENERATOR MODULES
-const newsDisplay = require("./newsGenerator")
+const newsInput = require("./newsInput")
+const newsFeed = require("./news")
 const eventDisplay = require("./eventsGenerator")
 const friendDisplay = require("./friendsGenerator")
 const messageDisplay = require("./messagesGenerator")
 const chatDisplay = require("./chat")
 const tasks = require("./tasks")
 const APIManager = require("./APIManager")
-const newsFeed = require("./news")
 
 
 // CALLS all GENERATOR functions
 // newsDisplay()
 // newsInput()
-// newsFeed()
+newsFeed()
 // eventDisplay()
 // friendDisplay()
 // taskDisplay()
