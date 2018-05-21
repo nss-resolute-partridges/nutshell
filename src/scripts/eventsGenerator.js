a9fac991fd224b3827c33a8444700a14a4ab779d
rafcevallos <rafael.cevallos81@gmail.com>
2018-05-21 10:59:01 -0500

    Merge conflicts hopefully fixed

--- src/scripts/eventsGenerator.js
+++ src/scripts/eventsGenerator.js
@@ -1,47 +1,82 @@
+/* Author: Raf  Used Jeremiah's code to create Add event button and push new event into the database. */
+
 // Factory functions required for EVENTS GENERATOR
+const fragment = document.createDocumentFragment()
 const $ = require("jquery")
+const eventSection = document.querySelector("#events")
+const eventCreationField = document.querySelector(".eventfields")
+const randomNumber = Math.floor(Math.random() * 100)
 const APIManager = require("./APIManager")
-// const componentFactory = require("./componentFactory")
+const componentFactory = require("./componentFactory")
 // const appendFactory = require("./appendFactory")
 
-// HTML Anchor where all elements will be attached
-// const contentHook = document.querySelector("#events")
-
-// This could change depending on what the group decides - in other words - we may not use fragments
-// const fragment = document.createDocumentFragment()
-
-// Function to loop through database to pull EVENTS and create DOM// Factory functions required for EVENTS GENERATOR
-
+//event listener attached to add event button and calls upon eventDisplay function to populate form to create new EVENT
+$(".eventCreationButton").on("click", function () {
+    $(".eventCreationButton").hide()
+    $("#events").append(eventDisplay())
+})
+//function to create input fields to create a new EVENT
 const eventDisplay = function () {
-    // Get the events
-    APIManager.getAllObjects("event")
-        .then(allEvents => {
-            // Iterate over event list
-            allEvents.forEach(event => {
-                // Build HTML representation for each one
-                console.log(event)
-                $("#events").append(
-                    `
-                    <div class="eventCard" id="${event.id}">
-                        <section>
-                            ${event.event}
-                        </section>
-                    </div>
-                    `
-                )
-                // const section = componentFactory("section", "", "eventCard")
-                // const name = componentFactory("h3", "", "eventCard")
+    const eventDiv = document.createElement("div")
+    eventDiv.classList = "eventField"
+    eventSection.appendChild(eventDiv)
 
-                // appendFactory(fragment, name)
-                // appendFactory(fragment, section)
+    /* Create input field for user to enter the event name */
+    // const eventName = document.createElement("input")
+    // eventName.type = "text"
+    // eventName.placeholder = "Event Name Here"
+    // eventName.classList = "eventNameField"
 
-                // appendFactory(contentHook, fragment)
+    /* Refactored the above to use componentFactory
+    Create input field for user to enter the EVENT name */
+    const eventName = componentFactory("input", "", "Event Name", "eventNameField")
+    eventDiv.appendChild(eventName)
 
-            })
-        })
+
+    const eventLocation = componentFactory("input", "", "Event Location", "eventLocationField")
+    eventDiv.appendChild(eventLocation)
+
+
+    // Would like to refactor this using componentFactory
+    const eventDate = document.createElement("input")
+    eventDate.type = "date"
+    eventDate.placeholder = "Event Date"
+    eventDate.classList = "event_date"
+    eventDiv.appendChild(eventDate)
+
+    /* Trying to refactor using componentFactory */
+    // const eventDate = componentFactory("input", "date", "Event Date", "event_date")
+
+    // const submitButton = document.createElement("button")
+    // submitButton.textContent = "Submit"
+    // submitButton.classList = "submitButton"
+
+    /* Refactored the above to use componentFactory
+    Create submit button for user to create EVENT in database */
+    const submitButton = componentFactory("button", "Submit", "", "submitButton")
+    eventDiv.appendChild(submitButton)
+
+    //event listener attached to submit button and when submit button is pressed clears form and shows add EVENT button
+    $(".submitButton").on("click", function () {
+        // input capture for object creation
+        let input = $(".eventNameField").val()
+        let inputDate = $(".event_date").val()
+        //object that is created from captured inputs
+        const eventObject = {
+            Id: (randomNumber),
+            eventName: (input),
+            eventDate: (inputDate),
+            status: "false",
+            userID: "",
+        }
+
+        //invoke post from api manager to push task object into database
+        APIManager.createObject("events", eventObject).then(
+            //event listeners that clear input fields and show the add EVENT button
+            $(".eventField").empty(),
+            $(".eventCreationButton").show()
+        )
+    })
 }
-// Call appendFactory to append all the created EVENTS elements above to the DOM
-// You will have to put in the arguments
-// appendFactory(parent, child)
 
-module.exports = eventDisplay
+// module.exports = eventDisplay
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,6 @@
 const newsDisplay = require("./newsGenerator")
 const eventDisplay = require("./eventsGenerator")
 const friendDisplay = require("./friendsGenerator")
-const taskDisplay = require("./tasksGenerator")
 const messageDisplay = require("./messagesGenerator")
 const tasks = require("./tasks")
 const APIManager = require("./APIManager")
@@ -12,22 +11,5 @@ const APIManager = require("./APIManager")
 // const newsInput = require("./newsInput")
 const newsFeed = require("./news")
 
-
-// CALLS all GENERATOR functions
-newsDisplay()
-newsInput()
-newsFeed()
-const eventDisplay = require("./eventsGenerator")
-const friendDisplay = require("./friendsGenerator")
-const taskDisplay = require("./tasksGenerator")
-const messageDisplay = require("./messagesGenerator")
-const chatDisplay = require("./chat")
-const register = require("./register")
-CALLS all GENERATOR functions
-newsDisplay()
-eventDisplay()
-friendDisplay()
-taskDisplay()
-messageDisplay()
 
 //chatDisplay()
