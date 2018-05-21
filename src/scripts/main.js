5313388b50fb86f922f9cc226833b1c67f7633ad
dan beecroft <binauralbeat001@gmail.com>
2018-05-21 13:39:05 -0500

    uncomented API manager vars

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,26 @@
 // REQUIRE all GENERATOR MODULES
-// const newsDisplay = require("./newsGenerator")
-// const eventDisplay = require("./eventsGenerator")
-// const friendDisplay = require("./friendsGenerator")
+const newsDisplay = require("./newsGenerator")
+const eventDisplay = require("./eventsGenerator")
+const friendDisplay = require("./friendsGenerator")
 const taskDisplay = require("./tasksGenerator")
 const messageDisplay = require("./messagesGenerator")
 const tasks = require("./tasks")
-// const APIManager = require("./APIManager")
-// const chatDisplay = require("./chat")
+const APIManager = require("./APIManager")
+const chatDisplay = require("./chat")
 const register = require("./register")
-
-
-
-// const newsDisplay = require("./newsGenerator")
-// const newsInput = require("./newsInput")
+const newsInput = require("./newsInput")
 const newsFeed = require("./news")
 
 
-// // CALLS all GENERATOR functions
-// newsDisplay()
-// newsInput()
-// newsFeed()
-// newsDisplay()
-// eventDisplay()
-// friendDisplay()
-// taskDisplay()
-// messageDisplay()
-//chatDisplay()
+// CALLS all GENERATOR functions
+newsDisplay()
+newsInput()
+newsFeed()
+newsDisplay()
+eventDisplay()
+friendDisplay()
+taskDisplay()
+messageDisplay()
+chatDisplay()
 
 chatDisplay()
